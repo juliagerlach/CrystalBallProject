eef7eb2c6b6ff672782916bda76d4b7fa2275ce8
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-16 07:47:45 -0500

    initial commit

---
+++ CrystalballProject/CrystalballProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrystalballProject.Startup))]
+namespace CrystalballProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
